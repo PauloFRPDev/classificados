3423a8d8e027c751200c8514cc0a14f03d76ef92
Paulo Felippe <paulofelipperp.dev@gmail.com>
2021-03-02 12:26:55 -0300

    model user

---
+++ src/models/User.ts
@@ -0,0 +1,31 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,26 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import User from '../models/User';
+
+@EntityRepository(User)
+class UsersRepository extends Repository<User> {
+  public async findById(id: string): Promise<User | null> {
+    const findUser = await this.findOne({
+      where: { id },
+    });
+
+    return findUser || null;
+  }
+
+  public async findByEmail(email: string): Promise<User | null> {
+    const findUser = await this.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return findUser || null;
+  }
+}
+
+export default UsersRepository;
