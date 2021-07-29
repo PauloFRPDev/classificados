f42e3f77370d159acbc2613c782891491d806981
Paulo Felippe <paulofelipperp.dev@gmail.com>
2021-07-29 16:22:36 -0300

    fix: update ad

--- src/services/UpdateAdService.ts
+++ src/services/UpdateAdService.ts
@@ -33,7 +33,7 @@ export default class UpdateAdService {
       throw new AppError('Ad not found.');
     }
 
-    if (!findAd.deleted_at !== null) {
+    if (findAd.deleted_at !== null) {
       throw new AppError('You can not update a deleted Ad.');
     }
 
